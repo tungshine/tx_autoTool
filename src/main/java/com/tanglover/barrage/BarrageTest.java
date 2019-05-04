9001cd096eee77389a82289b71a4c9fb698af8be
tungshine <tangxushine@163.com>
2019-05-04 20:09:36 +0800

    douyu barrage tool

--- src/main/java/com/tanglover/barrage/BarrageTest.java
+++ src/main/java/com/tanglover/barrage/BarrageTest.java
@@ -3,6 +3,7 @@ package com.tanglover.barrage;
 import com.yycdev.douyu.sdk.DouYuClient;
 import com.yycdev.douyu.sdk.MessageListener;
 import com.yycdev.douyu.sdk.entity.ChatMsg;
+import com.yycdev.douyu.sdk.entity.UenterMsg;
 
 /**
  * @author TangXu
@@ -13,21 +14,28 @@ public class BarrageTest {
 
 
     public static void main(String[] args) throws InterruptedException {
-        DouYuClient client = new DouYuClient("openbarrage.douyutv.com", 8601, "1693612");
+        DouYuClient client = new DouYuClient("openbarrage.douyutv.com", 8601, "485503");
         client.registerMessageListener(new MessageListener<ChatMsg>() {
             @Override
             public void read(ChatMsg message) {
-                System.out.println(message.toChatStr());
+//                System.out.println(message.toChatStr());
             }
         });
         client.registerMessageListener(new MessageListener<String>() {
             @Override
             public void read(String message) {
+//                System.out.println(message);
+            }
+        });
+
+        client.registerMessageListener(new MessageListener<UenterMsg>() {
+            @Override
+            public void read(UenterMsg message) {
                 System.out.println(message);
             }
         });
-        DouYuClient login = client.login();
-        DouYuClient sync = client.sync();
+        client.login();
+        client.sync();
         Thread.sleep(5000);
         client.exit();
     }
---
+++ src/main/java/com/tanglover/barrage/Login.java
@@ -0,0 +1,20 @@
+package com.tanglover.barrage;
+
+import javax.swing.*;
+
+/**
+ * @author TangXu
+ * @create 2019-05-04 19:48
+ * @description:
+ */
+public class Login {
+    private JPanel loginPanel;
+    private JLabel accountLabel;
+    private JTextField account;
+    private JPasswordField password;
+    private JLabel passwordLabel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("test");
+    }
+}
