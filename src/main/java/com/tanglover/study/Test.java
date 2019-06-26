9cfd5a2b793c71abae636311af7b375e34b165af
tungshine <362615233@qq.com>
2019-06-26 15:19:22 +0800

    autoTool

--- src/main/java/com/tanglover/study/Test.java
+++ src/main/java/com/tanglover/study/Test.java
@@ -3,7 +3,6 @@ package com.tanglover.study;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
-import java.util.Date;
 
 /**
  * @author TangXu
@@ -19,7 +18,8 @@ public class Test {
 
     @org.junit.Test
     public void test2() {
-        int a = 9;//定义一个变量；
+        //定义一个变量；
+        int a = 9;
         boolean b = (a < 4) && (a++ < 10);
         System.out.println("使用短路逻辑运算符的结果为" + b);
         System.out.println("a的结果为" + a);
